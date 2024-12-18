f48e02f56c4b947435d258b78002540c97c27b58
YashTheCoder <yashmdiwan@gmail.com>
2024-12-18 22:20:27 +0530

    Add Background component with initial UI layout

---
+++ src/components/Background.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Background.css";
+
+const Background = () => {
+  return (
+    <div className="background">
+      <h1>Welcome to My App</h1>
+      <p>This is an attractive UI example.</p>
+      <button className="primary-button">Click Me</button>
+    </div>
+  );
+};
+
+export default Background;
